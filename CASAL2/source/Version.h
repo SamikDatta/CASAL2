03769ef7a917159c4f1648f8efbf4d447c39fa6d
Samik Datta <samik.datta@niwa.co.nz>
2019-05-06 18:06:27 +1200

    UPD: Documentation, to show examples of retained and total catch observations, as well as the process MortalityInstantaneousRetained.

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION e65839ed2e88b7d356bd71a8dbbd52af40b39df6
-#define SOURCE_CONTROL_DATE "2019-05-05"
+#define SOURCE_CONTROL_REVISION 358bab11f7990876346788807e60092c28c7767b
+#define SOURCE_CONTROL_DATE "2019-05-06"
 #define SOURCE_CONTROL_YEAR "2019"
 #define SOURCE_CONTROL_MONTH "May"
-#define SOURCE_CONTROL_TIME "23:33:59"
-#define SOURCE_CONTROL_VERSION "2019-05-05 23:33:59 UTC (rev. e65839ed)"
+#define SOURCE_CONTROL_TIME "03:20:54"
+#define SOURCE_CONTROL_VERSION "2019-05-06 03:20:54 UTC (rev. 358bab11)"
 #endif
