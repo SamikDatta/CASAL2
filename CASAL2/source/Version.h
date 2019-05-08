8a965bb759243bc3c6800aa35a0b5aeee734f7e4
Teresa Amar <zta@uw.edu>
2019-05-09 11:05:32 +1200

    Update CppAD and Ipopt to 2016 release to compile with gcc 4.8.5

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 70ea952c2b7d2c371041ea4bf4bea83f097c74ea
-#define SOURCE_CONTROL_DATE "2019-05-07"
+#define SOURCE_CONTROL_REVISION 1c4409d47ba819b50ba7d4c1fc1105db0bd8aad0
+#define SOURCE_CONTROL_DATE "2019-05-08"
 #define SOURCE_CONTROL_YEAR "2019"
 #define SOURCE_CONTROL_MONTH "May"
-#define SOURCE_CONTROL_TIME "06:43:15"
-#define SOURCE_CONTROL_VERSION "2019-05-07 06:43:15 UTC (rev. 70ea952c)"
+#define SOURCE_CONTROL_TIME "08:05:53"
+#define SOURCE_CONTROL_VERSION "2019-05-08 08:05:53 UTC (rev. 1c4409d)"
 #endif
--- ThirdParty/cppad/windows.py
+++ ThirdParty/cppad/windows.py
@@ -6,15 +6,15 @@ from distutils import dir_util
 import Globals
 
 class Builder:
-  version_ = 3.11
-    
+  version_ = 3.12
+
   def start(self):
     # Variables
-    fileName         = 'msys'    
+    fileName         = 'msys'
     operatingSystem  = Globals.operating_system_
     libraries = [ "libcoinblas.a", "libcoinmetis.a", "libcoinmumps.a", "libcoinlapack.a", "libipopt.a" ]
     # Clean our any existing files if they already exist
-    print '-- Cleaning files'  
+    print '-- Cleaning files'
     if os.path.exists(fileName):
       os.system('chmod 777 -R ' + fileName)
       shutil.rmtree(fileName)
@@ -23,13 +23,13 @@ class Builder:
     os.system('rm -rf ' + Globals.target_include_path_ + "cppad")
     os.system('rm -rf ' + Globals.target_include_path_ + "coin")
     for library in libraries:
-      os.system('rm -rf ' + Globals.target_special_lib_path_ + library)        
-        
+      os.system('rm -rf ' + Globals.target_special_lib_path_ + library)
+
     # Decompress our archive
     print '-- Decompressing - check casal2_unzip.log'
     if os.path.exists(fileName + '.zip'):
       os.system('unzip ' + fileName + '.zip 1> casal2_unzip.log 2>&1')
-    
+
     # Build
     os.chdir(fileName + '\\1.0\\')
     print '-- Starting msys to decompress and build the IPOPT + CPPAD libary'
@@ -46,12 +46,12 @@ class Builder:
       time.sleep(1);
 
     time.sleep(5)
-    
+
     # Move our headers and libraries
     print '-- Moving headers and libraries'
     dir_util.copy_tree('cppad/include/cppad', Globals.target_include_path_ + '/cppad/')
-    dir_util.copy_tree('cppad/Ipopt-3.11.9/include/coin', Globals.target_include_path_ + '/coin/')
+    dir_util.copy_tree('cppad/Ipopt-releases-3.12.4/include/coin', Globals.target_include_path_ + '/coin/')
     for library in libraries:
-      shutil.copy('cppad/Ipopt-3.11.9/lib/' + library, Globals.target_special_lib_path_)    
-     
+      shutil.copy('cppad/Ipopt-releases-3.12.4/lib/' + library, Globals.target_special_lib_path_)
+
     return True
