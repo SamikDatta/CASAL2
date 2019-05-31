14e0082dbeec2fd448e174c891e85121333ff109
Samik Datta <samik.datta@niwa.co.nz>
2019-05-31 14:39:14 +1200

    FIX: ProportionsAtAge - when giving observations with aggregated categories, was crashing because of miscounting selectivity to use. Commented out lines 326-328, check with Craig.

--- CASAL2/source/Observations/Age/ProportionsAtAge.cpp
+++ CASAL2/source/Observations/Age/ProportionsAtAge.cpp
@@ -277,7 +277,8 @@ void ProportionsAtAge::Execute() {
    */
   unsigned selectivity_iter = 0;
 
-  LOG_FINEST() << "Number of categories " << category_labels_.size();
+  LOG_FINEST() << "Number of categories: " << category_labels_.size();
+  LOG_FINEST() << "Number of selectivities: " << selectivities_.size();
   for (unsigned category_offset = 0; category_offset < category_labels_.size(); ++category_offset, ++partition_iter, ++cached_partition_iter, ++selectivity_iter) {
     Double      selectivity_result = 0.0;
     Double      start_value        = 0.0;
@@ -294,6 +295,7 @@ void ProportionsAtAge::Execute() {
     auto category_iter = partition_iter->begin();
     auto cached_category_iter = cached_partition_iter->begin();
     for (; category_iter != partition_iter->end(); ++cached_category_iter, ++category_iter) {
+      LOG_FINEST() << "selectivity_iter = " << selectivity_iter;
     	if(selectivity_iter >= selectivities_.size())
     		LOG_CODE_ERROR() << "selectivity_iter > selectivities_.size()";
 
@@ -321,9 +323,9 @@ void ProportionsAtAge::Execute() {
         LOG_FINE() << "start_value: " << start_value << "; end_value: " << end_value << "; final_value: " << final_value;
         LOG_FINE() << "Numbers at age before ageing error is applied: " << numbers_age[data_offset];
       }
-    	if(selectivity_for_combined_categories_) {
-    		++selectivity_iter;
-    	}
+//    	if(selectivity_for_combined_categories_) {
+//    		++selectivity_iter;
+//    	}
     }
 
     /*
--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION cb5b27c7e8b390eac7de420e919829dce4614e76
-#define SOURCE_CONTROL_DATE "2019-05-22"
+#define SOURCE_CONTROL_REVISION a2cc3cc453dd43e30164310607ce8fa7daba5b00
+#define SOURCE_CONTROL_DATE "2019-05-24"
 #define SOURCE_CONTROL_YEAR "2019"
 #define SOURCE_CONTROL_MONTH "May"
-#define SOURCE_CONTROL_TIME "03:04:18"
-#define SOURCE_CONTROL_VERSION "2019-05-22 03:04:18 UTC (rev. cb5b27c7)"
+#define SOURCE_CONTROL_TIME "07:24:55"
+#define SOURCE_CONTROL_VERSION "2019-05-24 07:24:55 UTC (rev. a2cc3cc4)"
 #endif
